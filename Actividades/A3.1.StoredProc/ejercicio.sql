81b94ea94fcd6e954473e3ac85c7c72ae72ae499
eugenioLeal <eugenio.leal.333@gmail.com>
2018-01-17 21:24:23 -0600

    Cleaned up and Commented up :shower: + added history of commands typed in class

--- Actividades/A3.1.StoredProc/ejercicio.sql
+++ Actividades/A3.1.StoredProc/ejercicio.sql
@@ -1,6 +1,33 @@
-// primero
+--------------------------------------------------
+-- ejercicios de clase Miercoles 17/Enero/18    --
+-- Stop Procedures                              --
+--------------------------------------------------
+
+----------------------
+-- Ejemplo clase    --
+----------------------
+use classicmodels;
+Delimiter $$ 
+create procedure count_customers()
+BEGIN
+    declare contador int default 0;
+    select count(*) into contador from customers;
+    select contador;
+END$$
+Delimiter ;
+-- en otro archivo
+call count_customers();
+
+-- utiliza describe table_name para averiguar el tipo de dato almacenado
+describe products;
+
+--------------------------
+--  Primer Ejercicio    --
+--------------------------
+-- Muestra producto por “product line” (motorcycles)
 use classicmodels;
 delimiter $$
+-- Como ya sabes que linea_producto es un varchar(50) haces lo siguiente:
 create procedure show_products(
 IN linea_producto varchar(50))
 BEGIN 
@@ -10,8 +37,13 @@ BEGIN
     where productLine like line;
 END$$
 delimiter ;
+-- en otro archivo
+call show_products("Motor%");
 
-// segundo 
+--------------------------
+--  Segundo Ejercicio   --
+--------------------------
+-- Muestre cuantos clientes inicia su nombre con la letra “variable” (parametro)
 use classicmodels;
 delimiter $$
 create procedure show_clients(
@@ -23,10 +55,14 @@ BEGIN
     where nombre_cliente like line;
 END$$
 delimiter ;
-
+-- en otro archivo
 call show_clients("var%");
 
-// tercero
+--------------------------
+--  Tercer Ejercicio    --
+--------------------------
+-- Muestre el producto mas caro y el mas barato (no requiere parametros)
+-- intento uno
 use classicmodels;
 delimiter $$
 create procedure show_extreme_price()
@@ -35,7 +71,20 @@ BEGIN
     declare min1 decimal(10,2);
 	select max(buyPrice) into max1 from products;
 	select max(buyPrice) into max1 from products;    
-    select * from products
-    where buyPrice=min1 and buyPrice=max1;
+    select * from products where buyPrice=min1 or buyPrice=max1;
 END$$
 delimiter ;
+-- intento dos
+use classicmodels;
+delimiter $$
+create procedure show_extreme_price()
+BEGIN
+	declare max1 decimal(10,2);
+    declare min1 decimal(10,2);
+	select * from products order by buyPrice asc limit 1 into max1;
+	select * from products order by buyPrice asc limit 1 into max1;
+END$$
+delimiter ;
+-- en otro archivo
+call show_extreme_price();
+
